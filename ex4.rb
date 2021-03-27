43381af3727df690798543f89159531a1d46d193
DochkinVitaliy <DochkinVitaliy@users.noreply.github.com>
2021-03-27 14:49:11 +0300

    lab 1 zad 4

--- ex4.rb
+++ ex4.rb
@@ -1,23 +1,4 @@
-puts "Введите команду: (0 - выход)"
-
-input = gets.chomp
-mass = input.split
-
-if mass.size == 1
-	exec(mass[0])
-elsif mass.size == 2
-	exec(mass[0], mass[1])
-else
-	puts "Я могу читать только 1 аргумент."
-end
-while input != "0"
-	input = gets.chomp
-	mass = input.split
-	if mass.size == 1
-		exec(mass[0])
-	elsif mass.size == 2
-		exec(mass[0], mass[1])
-	else
-		puts "Я могу читать только 1 аргумент."
-	end
-end
+print "Введите команду вашей операционной системы ч/з system (только не надо rm -rf /): "
+system STDIN.gets
+print "Введите команду Ruby для исполнения: "
+eval STDIN.gets
